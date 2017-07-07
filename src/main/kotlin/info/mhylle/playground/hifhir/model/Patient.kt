f19840d3e6333beec2af18277d4204dc1cec6b9d
Martin Hylleberg <mhylle@yahoo.com>
2017-07-07 08:12:23 +0200

    starting to create patient data class

--- src/main/kotlin/info/mhylle/playground/hifhir/model/Patient.kt
+++ src/main/kotlin/info/mhylle/playground/hifhir/model/Patient.kt
@@ -1,13 +1,3 @@
 package info.mhylle.playground.hifhir.model
 
-import java.time.LocalDateTime
-import java.util.*
-
-class Patient {
-    var id: UUID = UUID.randomUUID()
-    var identifier: String = ""
-    var firstName: String = ""
-    var middleName : String = ""
-    var lastName : String = ""
-    lateinit var birthDate : LocalDateTime
-}
+data class Patient(val id: String, val firstName: String, val middleName: String, val familyName: String)
